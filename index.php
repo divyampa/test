0feddc6c5abf61b9752dfe4d8a1295d726687a47
divyampa <43271665+divyampa@users.noreply.github.com>
2020-09-29 12:21:59 +0530

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo ' 
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<img src=" " width="280" height="125" title="My site" alt=" My public site" />
-
-</html>
-';
-?>
